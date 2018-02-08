9e4dc6cc4b842ede41b1480f6fee15fa8de4ac84
Saaheel Majethia <saaheel.m@peopleinteractive.in>
2018-02-08 12:28:54 +0530

    Initial Commit

---
+++ helloworld.py
@@ -0,0 +1,33 @@
+print("hello world")
+
+
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = round(num1) + round(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum));
+
+
+
+
+
+fname = "sahil";
+lname = "majethia";
+
+
+print('{0} and {1}'.format(fname,lname));
+
+
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum));
